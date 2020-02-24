f7a3aaa04a650a64edf79a0b3701e00cd21d497b
aldousWatts <aldouswatts@protonmail.com>
2020-02-24 01:49:48 -0600

    scavenging thieving calc

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ## This is Script Engine for Melvor Idle v0.1.
 Download from Mozilla add-on database coming soon.
 ### It's a Firefox browser extension for the fun idle web game Melvor Idle. It helps you automate certain aspects of the game.
-* Auto-combat: eat food and loot automatically, exit combat if you're out of food/ammo/runes.
+* AutoCombat: eat food and loot automatically, exit combat if you're out of food/ammo/runes.
 * AutoCombat Options: Toggle auto eat and loot
+* AutoSlayer by Bubbalova: automates Slayer tasks, optionally automatically equips mirror shield or magic ring.
 * Auto-cook: permanently unlocked Cook All button in the Cooking skill page
 * XHP script by Breakit
 * Calculate number of selected potions needed until next level button in Herblore page
+* Thieving calculators by RedSparr0w (scavenged from Melvor Idle Helper)
 
 **Temporary install:**
 Download the repository, including SEMI.js, manifest.json, inject.js, and the icons folder. 
--- inject.js
+++ inject.js
@@ -422,6 +422,59 @@ var smithingHUD = window.setInterval(function() {
 //::*/
 //hmmm... numberWithCommas eh? is this a game function?
 
+//:: Scavenging the Thieving calculator from Melvor Idle Helper by RedSparr0w on GitLab: https://github.com/RedSparr0w/Melvor-Idle-Helper
+const waitForPage = setInterval(() => {
+    const changeLog = document.getElementById('changelog-body-container');
+    if (!changeLog || !changeLog.innerText) return;    
+    clearInterval(waitForPage);// Page is loaded
+    thievingCalc();
+    $('.js-popover').popover({ // Enable the popovers
+        container: 'body',
+        animation: false,
+        trigger: 'hover focus',
+    });
+}, 200);
+
+const addCalcToEl = (el, data = []) => {
+    if (!el || !el.appendChild) return;    
+    const helper_container = document.createElement('div'); // create our helper elements
+    helper_container.className = 'font-size-sm font-w600 text-right text-uppercase text-muted';
+    helper_container.style = 'position: absolute; right: 6px; top: 8px;';
+    data.forEach((dat, i)=>{
+        // Add line break if not first element
+        if (i > 0) helper_container.appendChild(document.createElement('br'));
+        const el = document.createElement('small');
+        el.innerText = dat;
+        helper_container.appendChild(el);
+    });
+    el.classList.add('ribbon', 'ribbon-light', 'ribbon-bookmark', 'ribbon-left'); // Needs these classes for the text to show correctly
+    el.appendChild(helper_container);
+}
+
+//this is freshhh - Thieving Calc & Item Popup! SO Handy!
+const thievingCalc = () => {
+    const seconds = baseThievingInterval / 1000; // Always takes the same amount of time
+    thievingNPC.forEach((npc, id) => {
+        const xp_ps = +(npc.xp / seconds).toFixed(1);
+        // Get the loottable text
+        let popoutText = [`<img src='http://melvoridle.com/assets/media/main/coins.svg' height='20px'> ${npc.maxCoins} coins (max)`];
+        const totalWeight = npc.lootTable.reduce((a,b)=>a + b[1], 0);
+        npc.lootTable.forEach(loot => {
+            const item = items[loot[0]];
+            popoutText.push(`<img src='${item.media}' height='20px'> ${item.name} - ${((loot[1] / totalWeight) * 100).toFixed(1)}%`);
+        });
+        const npc_el = document.getElementById(`thieving-npc-${id}`).getElementsByClassName('block-content')[0];
+        addCalcToEl(npc_el, [xp_ps + ' XP/s']); // Add the xp/s amounts
+        npc_el.classList.add('js-popover'); // Add the popovers for the loot
+        const npc_el_data = npc_el.dataset;
+        npc_el_data.toggle = 'popover';
+        npc_el_data.html = 'true';
+        npc_el_data.placement = 'bottom';
+        npc_el_data.content = popoutText.join('<br/>');
+    });
+}
+//:: end of import of scraps from Melvor Idle Helper
+
 //The End
 //SEMI loaded custom notification
 if(navigator.userAgent.match("Chrome")){
