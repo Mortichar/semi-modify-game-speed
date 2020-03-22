dbadae474ff99160d9acb7b516c1d45b6c36ed6d
aldousWatts <aldouswatts@protonmail.com>
2020-03-22 00:54:23 -0500

    added confirm prompt to destroy crops buttons

--- inject.js
+++ inject.js
@@ -1199,12 +1199,15 @@ function toggleAutoReplant() {
 
 //adding destroy crop functionality by Jarx in discord, modified by me based on game's source
 function destroyCrops() {
-    for (let i = 0; i < newFarmingAreas[currentFarmingArea].patches.length; i++) {
-        if (newFarmingAreas[currentFarmingArea].patches[i].seedID > 0) {
-            removeSeed(newFarmingAreas[currentFarmingArea].id, i);
+    var confirmDestroyCrops = propmpt("Wait. Are you sure you want to continue? Type yes in the prompt to destroy crops.", "wait");
+    if (confirmDestroyCrops === "yes") {
+        for (let i = 0; i < newFarmingAreas[currentFarmingArea].patches.length; i++) {
+            if (newFarmingAreas[currentFarmingArea].patches[i].seedID > 0) {
+                removeSeed(newFarmingAreas[currentFarmingArea].id, i);
+            }
         }
+        customNotify('assets/media/skills/farming/farming.svg', 'SEMI destroyed all of your '+newFarmingAreas[currentFarmingArea].areaName+'.', 4000);
     }
-    customNotify('assets/media/skills/farming/farming.svg', 'SEMI destroyed all of your '+newFarmingAreas[currentFarmingArea].areaName+'.', 4000);
 }
 
 //adding button to the farming page to destroy crops
