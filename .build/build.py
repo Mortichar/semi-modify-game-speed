733dc5f30c601ef0992da3528567185f9b5738fd
AldousWatts <aldouswatts@protonmail.com>
2020-08-30 06:19:34 -0500

    fix build.py errors

--- .build/build.py
+++ .build/build.py
@@ -16,7 +16,7 @@ def main():
     # define the three directories we need to add to archive
     rootDir = os.getcwd()
     folder1 = "/icons"
-    folder2 = "/scripts"
+    folder2 = "/scripts/plugins"
     folder3 = "/styles"
     dir1 = rootDir+folder1
     dir2 = rootDir+folder2
@@ -26,25 +26,46 @@ def main():
     print('Building SEMI-Firefox.zip...')
     with ZipFile('SEMI-Firefox.zip', 'w') as fBuild:
         fBuild.write('manifest.json')
+        os.chdir('scripts')
+        fBuild.write('SEMI.js', '/scripts/SEMI.js')
+        fBuild.write('core.js', '/scripts/core.js')
+        fBuild.write('utils.js', '/scripts/utils.js')
         # add the 3 SEMI subdirectories: icons, scripts, and styles
         zipdir(dir1, fBuild, folder1)
         zipdir(dir2, fBuild, folder2)
         zipdir(dir3, fBuild, folder3)
 
+    # Return to SEMI root directory for chrome
+    os.chdir(os.pardir)
+
     # Create chrome output zip
     print('Building SEMI-Chrome.zip...')
     with ZipFile('SEMI-Chrome.zip', 'w') as cBuild:
         cBuild.write('chrome.manifest.json', 'manifest.json')
+        os.chdir('scripts')
+        cBuild.write('SEMI.js', '/scripts/SEMI.js')
+        cBuild.write('core.js', '/scripts/core.js')
+        cBuild.write('utils.js', '/scripts/utils.js')
         # add the 3 SEMI subdirectories: icons, scripts, and styles
         zipdir(dir1, cBuild, folder1)
         zipdir(dir2, cBuild, folder2)
         zipdir(dir3, cBuild, folder3)
 
-    # Move the two files to ./.build/outputs/
+    # Check for outputs directory, make it if doesn't exist
     print("Checking for ./.build/outputs directory and creating if it doesn't exist...")
     if not os.path.exists(rootDir+"/.build/outputs"):
         os.makedirs(rootDir+"/.build/outputs")
 
+    # Remove previous outputs
+    if os.path.exists(rootDir+"/.build/outputs/SEMI-Firefox.zip"):
+        print("Removing previous SEMI-Firefox.zip...")
+        os.remove(rootDir+"/.build/outputs/SEMI-Firefox.zip")
+
+    if os.path.exists(rootDir+"/.build/outputs/SEMI-chrome.zip"):
+        print("Removing previous SEMI-chrome.zip...")
+        os.remove(rootDir+"/.build/outputs/SEMI-chrome.zip")
+
+    # Move the two files to ./.build/outputs/
     print("Moving SEMI-Firefox.zip and SEMI-Chrome.zip to ./.build/outputs/...")
     os.rename(rootDir+"/SEMI-Firefox.zip", rootDir+"/.build/outputs/SEMI-Firefox.zip")
     os.rename(rootDir+"/SEMI-Chrome.zip", rootDir+"/.build/outputs/SEMI-Chrome.zip")
