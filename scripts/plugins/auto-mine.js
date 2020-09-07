b51732fec49bcb528caa6928adeefbe7a3a331de
Visua <6732520-visua0@users.noreply.gitlab.com>
2020-09-07 04:59:02 +0000

    AutoMine update

--- scripts/plugins/auto-mine.js
+++ scripts/plugins/auto-mine.js
@@ -6,92 +6,125 @@
     const skill = 'Mining';
 
     const menuConfig = {};
+
+    const storeConfig = () => {
+        SEMI.setItem('AM-priority-config', menuConfig);
+    };
+
     const storeAutoMineOrder = () => {
-        const menu = $("#SEMI-Auto-Mine-drag-div");
+        const menu = $('#SEMI-Auto-Mine-drag-div');
         menuConfig['AM'].order = [...menu.children()].map((x) => x.id);
-        SEMI.setItem("AM-priority-config", menuConfig);
+        storeConfig();
         orderMenu();
     };
+
     const indexOrInf = (arr) => (el) => {
         let i = arr.indexOf(el);
         return i === -1 ? Infinity : i;
     };
+
     const orderMenu = () => {
-        const menu = $("#SEMI-Auto-Mine-drag-div");
+        const menu = $('#SEMI-Auto-Mine-drag-div');
         const order = menuConfig['AM'].order;
-        if(order.length === 0) {return;}
+        if (order.length === 0) {
+            return;
+        }
         const menuItems = [...menu.children()];
         const indexOf = indexOrInf(order);
         const sortedMenu = menuItems.sort((menuA, menuB) => indexOf(menuA.id) - indexOf(menuB.id));
         menu.append(sortedMenu);
     }
-    const injectAutoMineGUI = () => {
-        const ores = ['copper', 'tin', 'iron', 'coal', 'silver', 'gold', 'mithril', 'adamantite', 'runite', 'dragonite', 'rune_essence'];
-        menuConfig['AM'] = {locked: false, order: []};
 
-        const div = (i) => {
-            if (i==10) {
-                return `<div id="AutoMineDrag${i}" class="AMbtn btn btn-outline-primary SEMI-AM-els" title="Rune Essence">
-                <img src="assets/media/bank/${ores[i]}.svg" width="36" height="36">
-                </div>`;
-            } else {
-                return `<div id="AutoMineDrag${i}" class="AMbtn btn btn-outline-primary SEMI-AM-els" title="${ores[i].charAt(0).toUpperCase() + ores[i].slice(1)} Ore">
-                <img src="assets/media/skills/mining/rock_${ores[i]}.svg" width="36" height="36">
-                </div>`;
-            }
+    const injectAutoMineGUI = () => {
+        menuConfig['AM'] = { ignoreCoal: false, order: [] };
+        const storedMenuConfig = SEMI.getItem("AM-priority-config");
+        if (storedMenuConfig !== null) {
+            menuConfig['AM'] = storedMenuConfig['AM'];
         }
 
-        const divs = Array(11).fill(0).map((_, i) => div(i)).join('');
+        const ores = ['copper', 'tin', 'iron', 'coal', 'silver', 'gold', 'mithril', 'adamantite', 'runite', 'dragonite', 'rune_essence'];
+        const div = (i) => {
+            let title = `${ores[i].charAt(0).toUpperCase() + ores[i].slice(1)} Ore`;
+            let src = `assets/media/skills/mining/rock_${ores[i]}.svg`;
+            if (i === 10) {
+                title = 'Rune Essence';
+                src = `assets/media/bank/${ores[i]}.svg`;
+            }
+            return `<div id="AutoMineDrag${i}" class="AMbtn btn btn-outline-primary" style="padding: 2px ; margin: 2px;" data-tippy-content="${title}"><img src="${src}" width="36" height="36"></div>`;
+        }
 
-        const selector = `
-        <div class="col-6 col-lg-12" id="AMselector">
-            <div class="block-header mb-1 SEMI-gold SEMI-AM-info" style="">
-                SEMI AutoMine Priority Setting GUI: Set priority directly by dragging.<br>
-                <-- Highest Priority || Lower Priority -->
+        const bars = ['bronze', 'iron', 'steel', 'silver', 'gold', 'mithril', 'adamantite', 'runite', 'dragonite'];
+        const btn = (i) => {
+            let src = `assets/media/bank/${bars[i]}_bar.svg`;
+            if (i === bars.length) {
+                src = 'assets/media/shop/pickaxe_dragon.svg';
+            }
+            return `<div id="AMbtn${i}" class="AMbtn btn btn-outline-primary" style="padding: 2px ; margin: 2px;"><img src="${src}" width="36" height="36"></div>`;
+        };
+
+        const barOptions = `
+            <div class="custom-control custom-checkbox mb-1">
+                <input type="checkbox" class="custom-control-input" id="${id}-ignore-coal" name="${id}-ignore-coal"${menuConfig['AM'].ignoreCoal ? ' checked="checked"' : ''}>
+                <label class="custom-control-label" for="${id}-ignore-coal" data-tippy-content="For Superheat IV">Ignore coal</label>
             </div>
-        </div>`
+            <div>${bars.map((_, i) => btn(i)).join('') + btn(bars.length)}</div>`;
 
-        $('#mining-container .row:first').after($(`${selector}<div id="SEMI-Auto-Mine-drag-div" class="col-6 col-lg-12">${divs}</div><br><br>`));
+        const createBlock = (title, description, content) => {
+            return `
+                <div class="col-md-6">
+                    <div class="block block-rounded block-link-pop border-top border-mining border-4x" style="padding-bottom: 12px; display: flex; flex-direction: column">
+                        <div class="block-header text-center"><h3 class="block-title">${title}</h3></div>
+                        <div class="block-content text-center font-w400 font-size-sm" style="padding-top: 4px;">${description}</div>
+                        <div class="block-content text-center" style="padding-top: 12px; margin-top: auto;">${content}</div>
+                    </div>
+                </div>`;
+        };
+
+        const prioritySetting = createBlock(
+            'AutoMine Priority Setting',
+            'Set priority directly by dragging.<br><-- Highest Priority || Lower Priority -->',
+            `<div id="SEMI-Auto-Mine-drag-div">${ores.map((_, i) => div(i)).join('')}</div>`);
+
+        const barSelection = createBlock(
+            'AutoMine Bar Selection',
+            'Mine by ratios of resources in your bank.<br>Ex: 4 coal per 1 mithril. Senses Smithing cape and halves coal requirements if owned.<br>Click current selection to de-select and return to priority-based mining.',
+            barOptions);
+
+        const autoMineDiv = `<div id="${id}" class="row row-deck">${prioritySetting}${barSelection}</div>`;
+
+        $('#mining-container .row:first').after($(autoMineDiv));
+
+        for (let i = 0; i < bars.length + 1; i++) {
+            $(`#AMbtn${i}`).on('click', () => AMselect(i));
+        }
+
+        $(`#${id}-ignore-coal`).change(() => {
+            menuConfig['AM'].ignoreCoal = event.target.checked;
+            storeConfig();
+        });
+
+        // tool-tip for bars with capital letters [by Remko de Bruin]
+        for (let i = 0; i < bars.length; i++) {
+            $(`#AMbtn${i}`).attr('data-tippy-content', `${bars[i].charAt(0).toUpperCase() + bars[i].slice(1)} Bar`);
+        }
+        $(`#AMbtn${bars.length}`).attr('data-tippy-content', 'Default AM mineArray setting: prioritize XP');
 
         const makeSortable = (id) => {
             Sortable.create(document.getElementById(id), {
                 animation: 150,
                 filter: '.SEMI-locked',
-                onEnd: storeAutoMineOrder}); };
+                onEnd: storeAutoMineOrder
+            });
+        };
         makeSortable('SEMI-Auto-Mine-drag-div');
 
-        const storedMenuConfig = SEMI.getItem("AM-priority-config");
-        if (storedMenuConfig !== null) {
-            menuConfig['AM'] = storedMenuConfig['AM'];
-            orderMenu();
-        }
+        tippy(`#${id} [data-tippy-content]`, { animation: false });
 
-        const bars = ['bronze', 'iron', 'steel', 'silver', 'gold', 'mithril', 'adamantite', 'runite', 'dragonite'];
-        const btn = (i) => `<button id="AMbtn${i}" class="AMbtn btn btn-outline-primary SEMI-AM-els" type="button">
-        <img src="assets/media/bank/${bars[i]}_bar.svg" width="36" height="36">
-        </button>`;
-
-        const btns = Array(10).fill(0).map((_, i) => btn(i)).join('');
-        const barSelector = `
-        <div class="col-6 col-lg-12" id="AMselector">
-            <div class="block-header mb-1 SEMI-gold SEMI-AM-info">
-                Bar Selection: Mine by ratios of resources in your bank.<br>
-                Ex: 4 coal per 1 mithril. Senses Smithing cape & halves coal requirements if owned.<br>
-                Click current selection to de-select and return to priority-based mining.
-            </div>
-        </div>`
-        //AutoMine Bar Select GUI
-        $('#SEMI-Auto-Mine-drag-div').after($(`${barSelector}<div class="col-6 col-lg-12">${btns}</div>`));
-        for(let i = 0; i < bars.length + 1; i++) { $(`#AMbtn${i}`).on('click', () => AMselect(i)); }
-        //tool-tip for bars with capital letters [by Remko de Bruin]
-        for (let i = 0; i < bars.length; i++){ $(`#AMbtn${i}`).attr('title', `${bars[i].charAt(0).toUpperCase() + bars[i].slice(1)} Bar`); }
-        $(`#AMbtn${9}`).attr('title', "Default AM mineArray setting: prioritize XP.");
-        $(`#AMbtn${9}`).find('img').attr('src', "assets/media/shop/pickaxe_dragon.svg");
-        if (AMselection !== -1) { highlightBarBtn(AMselection); }
-
+        highlightBarBtn(AMselection);
+        orderMenu();
     };
 
-    const ORES = {Copper: 0, Tin: 1, Iron: 2, Coal: 3, Silver: 4, Gold: 5, Mithril: 6, Adamantite: 7, Runite: 8, Dragonite: 9, RuneEssence: 10};
+    const ORES = { Copper: 0, Tin: 1, Iron: 2, Coal: 3, Silver: 4, Gold: 5, Mithril: 6, Adamantite: 7, Runite: 8, Dragonite: 9, RuneEssence: 10 };
 
     const oreIDs = [45, 46, 47, 48, 49, 50, 51, 52, 53, 54];
 
@@ -105,53 +138,59 @@
         [ORES.Adamantite, ORES.Coal], // addy bar
         [ORES.Runite, ORES.Coal], // rune bar
         [ORES.Dragonite, ORES.Runite, ORES.Coal], // dragon bar (snack legendarily ®)
-        [ORES.Dragonite, ORES.Runite, ORES.Adamantite, ORES.Mithril, ORES.Gold, ORES.Silver, ORES.Coal, ORES.Iron, ORES.Tin, ORES.Copper, ORES.RuneEssence], //xp priority
-        [ORES.RuneEssence]
+        [ORES.Dragonite, ORES.Runite, ORES.Adamantite, ORES.Mithril, ORES.Gold, ORES.Silver, ORES.Coal, ORES.Iron, ORES.Tin, ORES.Copper, ORES.RuneEssence], // xp priority
+        [ORES.RuneEssence],
     ];
 
     let AMselection = -1;
 
     const barRatios = [
-        {[ORES.Copper]: 1, [ORES.Tin]: 1},
-        {[ORES.Iron]: 1},
-        {[ORES.Iron]: 1, [ORES.Coal]: 2},
-        {[ORES.Silver]: 1},
-        {[ORES.Gold]: 1},
-        {[ORES.Mithril]: 1, [ORES.Coal]: 4},
-        {[ORES.Adamantite]: 1, [ORES.Coal]: 6},
-        {[ORES.Runite]: 1, [ORES.Coal]: 8},
-        {[ORES.Dragonite]: 1, [ORES.Runite]: 2, [ORES.Coal]: 12}
+        { [ORES.Copper]: 1, [ORES.Tin]: 1 },
+        { [ORES.Iron]: 1 },
+        { [ORES.Iron]: 1, [ORES.Coal]: 2 },
+        { [ORES.Silver]: 1 },
+        { [ORES.Gold]: 1 },
+        { [ORES.Mithril]: 1, [ORES.Coal]: 4 },
+        { [ORES.Adamantite]: 1, [ORES.Coal]: 6 },
+        { [ORES.Runite]: 1, [ORES.Coal]: 8 },
+        { [ORES.Dragonite]: 1, [ORES.Runite]: 2, [ORES.Coal]: 12 },
     ];
-    let resourceRatios = [];
-    
+
+    let hasSmithingCape = false;
+
     /** @param {number} n */
     const highlightBarBtn = (n) => {
-        $('.AMbtn').removeClass('btn-primary'); //de-highlight all
-        $(`#AMbtn${n}`).addClass('btn-primary'); //highlight n
+        $('.AMbtn').removeClass('btn-primary'); // de-highlight all
+        $(`#AMbtn${n}`).addClass('btn-primary'); // highlight n
     };
-
 
     /**
      * autoMine Selector: GUI for choosing bars to mine for.
      * @param {number} n
      */
     const AMselect = (n) => {
-        if (n == AMselection) { n = -1; }
+        if (n === AMselection) {
+            n = -1;
+        }
         highlightBarBtn(n);
         AMselection = n;
-        if (n !== -1) { mineArray = mineArrays[n]; }
+        if (n !== -1) {
+            mineArray = mineArrays[n];
+        }
     };
 
-    //AutoMine: Will mine based on your or priorities set in mineArray //aw: this still works awesomely!
+    // AutoMine: Will mine based on your or priorities set in mineArray // aw: this still works awesomely!
     const onEnable = () => {
-        if (!SEMI.isCurrentSkill(skill)) { mineRock(0); }
+        if (!SEMI.isCurrentSkill(skill)) {
+            mineRock(0);
+        }
         injectAutoMineGUI();
+        hasSmithingCape = SEMI.hasCapeOn('Smithing') || SEMI.ownsCape('Smithing');
     };
 
     const onDisable = () => {
         SEMI.stopSkill(skill);
         removeAutoMineGUI();
-        // SEMI.removeAutoMineGUI();
     };
 
     let mineArray = [ORES.Dragonite, ORES.Runite, ORES.Adamantite, ORES.Mithril, ORES.Gold, ORES.Silver, ORES.Coal, ORES.Iron, ORES.Tin, ORES.Copper, ORES.RuneEssence];
@@ -159,42 +198,47 @@
     /** @param {number[]} rocks */
     const autoMine = (rocks = mineArray) => {
         let swingRatio = 0;
-        if (SEMI.isCurrentSkill(skill)) { swingRatio = Number(document.getElementById(`mining-rock-progress-${currentRock}`).style.width.split('%')[0]); }
+        if (SEMI.isCurrentSkill(skill)) {
+            swingRatio = Number(document.getElementById(`mining-rock-progress-${currentRock}`).style.width.split('%')[0]);
+        }
         if (AMselection !== 9 && AMselection !== -1) {
+            let resourceRatios = [];
             for (const rock of rocks) {
-                const wearingCape = SEMI.currentEquipmentInSlot('Cape') == CONSTANTS.item.Smithing_Skillcape;
-                const ownsCape = SEMI.ownsCape('Smithing');
-                resourceRatios[rock] = SEMI.getBankQty(oreIDs[rock])/(barRatios[AMselection][rock]);
-                if ((wearingCape || ownsCape) && rock == [ORES.Coal]) { resourceRatios[rock] *= 2; }
+                if (rock === ORES.Coal && menuConfig['AM'].ignoreCoal) {
+                    resourceRatios[rock] = Infinity;
+                } else {
+                    resourceRatios[rock] = SEMI.getBankQty(oreIDs[rock]) / (barRatios[AMselection][rock]);
+                    if (rock === ORES.Coal && hasSmithingCape) {
+                        resourceRatios[rock] *= 2;
+                    }
+                }
             }
             mineArray.sort((a, b) => resourceRatios[a] - resourceRatios[b]);
         }
         const order = menuConfig['AM'].order;
-        if(order.length !== 0 && AMselection == -1) {
+        if (order.length !== 0 && AMselection === -1) {
             const indexOf = indexOrInf(order);
             mineArray = [ORES.Dragonite, ORES.Runite, ORES.Adamantite, ORES.Mithril, ORES.Gold, ORES.Silver, ORES.Coal, ORES.Iron, ORES.Tin, ORES.Copper, ORES.RuneEssence];
-            mineArray.sort((a,b) => indexOf(`AutoMineDrag${a}`) - indexOf(`AutoMineDrag${b}`));
+            mineArray.sort((a, b) => indexOf(`AutoMineDrag${a}`) - indexOf(`AutoMineDrag${b}`));
         }
-        for(const rock of rocks) {
-            if (!SEMI.isCurrentSkill(skill)) mineRock(rock);
-            if(!rockData[rock].depleted && miningData[rock].level <= SEMI.currentLevel(skill)) { //added extra condition to make universal
-                if(currentRock !== rock && (swingRatio<10) ) { mineRock(rock); }
-                return; //necessary to stop it looping all the way to last rock. i see now.
+        for (const rock of rocks) {
+            if (!SEMI.isCurrentSkill(skill)) {
+                mineRock(rock);
+            }
+            if (!rockData[rock].depleted && miningData[rock].level <= SEMI.currentLevel(skill)) { // added extra condition to make universal
+                if (currentRock !== rock && (swingRatio < 10)) {
+                    mineRock(rock);
+                }
+                return; // necessary to stop it looping all the way to last rock. i see now.
             }
         }
         storeAutoMineOrder();
     };
 
     const removeAutoMineGUI = () => {
-        // $("#AMbtn0").parent().remove();
-        $("#AMselector").nextUntil('#mining-ores-container').remove();
-        $("#AMselector").remove();
-        $("[id^='autoMine']").remove();
+        $(`#${id}`).remove();
+        $(`#${id} [data-tippy-content]`).each((_, e) => e._tippy.destroy());
     };
 
-    // const mergeObj = {removeAutoMineGUI};
-    //injectGUI: injectAutoMineGUI,
-    SEMI.add(id, {ms: 100, onLoop: autoMine, onEnable, onDisable, desc, title, imgSrc, skill});
-
-    // SEMI.mergeOnto(SEMI, {removeAutoMineGUI});
+    SEMI.add(id, { ms: 100, onLoop: autoMine, onEnable, onDisable, desc, title, imgSrc, skill });
 })();
