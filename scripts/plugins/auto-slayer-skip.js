954de0fdf0cbd052d1f8e4ea87d71c41751cdeb0
Visua <6732520-visua0@users.noreply.gitlab.com>
2020-09-23 23:16:01 +0000

    AutoSlayer fixes and improvements

--- scripts/plugins/auto-slayer-skip.js
+++ scripts/plugins/auto-slayer-skip.js
@@ -6,33 +6,28 @@ var autoSellShow = (() => {
     const desc = 'This script option for AutoSlayer will skip all monsters selected in this menu.';
     const imgSrc = 'assets/media/monsters/m13.svg';
 
+    let showHiddenMonsters = false;
     let autoEnabled = [];
 
-    const fadeAll = () => {
-        for (let i = 0; i < monsterStats.length; i++) {
-            const x = $(`#${id}-img-${i}`);
-            if(x.length === 0) { continue; }
-            const shouldBeFaded = !autoEnabled[i];
-            const currentState = typeof x.css('opacity') === 'undefined' ? '1' : x.css('opacity');
-            const isFaded = currentState === '0.25';
-            const isCorrect = isFaded === shouldBeFaded;
-            const neededOpacity = shouldBeFaded ? 0.25 : 1;
-            if(!isCorrect) { x.fadeTo(500, neededOpacity); }
-        }
+    /** @param {number} monsterId */
+    const getNeededOpacity = (monsterId) => {
+        return autoEnabled[monsterId] ? 1 : 0.25;
     };
 
-    /** @param {number} i */
-    const toggleAutoEnabled = (i) => {
-        autoEnabled[i] = !autoEnabled[i];
-        fadeAll();
+    /** @param {number} monsterId */
+    const toggleAutoEnabled = (monsterId) => {
+        autoEnabled[monsterId] = !autoEnabled[monsterId];
+        $(`#${id}-img-${monsterId}`).fadeTo(500, getNeededOpacity(monsterId));
     };
 
     const emptyObj = {media: 'assets/media/main/question.svg', name: '???'};
-    const el = (i) => {
-        const empty = monsterStats[i].killedByPlayer === 0;
-        const {media, name} = empty ? emptyObj : MONSTERS[i];
-        const e = $(`<img id="${id}-img-${i}" class="skill-icon-md js-tooltip-enable" src="${media}" data-toggle="tooltip" data-html="true" data-placement="bottom" title="" data-original-title="${name}">`);
-        if(!empty) { e.on('click', () => toggleAutoEnabled(i)); }
+
+    /** @param {number} monsterId */
+    const createImg = (monsterId) => {
+        const empty = monsterStats[monsterId].killedByPlayer === 0 && !showHiddenMonsters;
+        const {media, name} = empty ? emptyObj : MONSTERS[monsterId];
+        const e = $(`<img id="${id}-img-${monsterId}" class="skill-icon-md" src="${media}" data-tippy-content="${name}" data-tippy-placement="bottom" style="opacity: ${getNeededOpacity(monsterId)};">`);
+        if(!empty) { e.on('click', () => toggleAutoEnabled(monsterId)); }
         return e;
     };
 
@@ -41,18 +36,15 @@ var autoSellShow = (() => {
     };
 
     const setupContainer = () => {
+        $(`#${id}-container [data-tippy-content]`).each((_, e) => e._tippy.destroy());
         $(`#${id}-container`).html('');
-        autoEnabled = Array(monsterStats.length).fill(false);
-        for (let i = 0; i < monsterStats.length; i++) {
-            $(`#${id}-container`).append(el(i));
+
+        const slayerTasks = combatAreaDisplayOrder.flatMap(area => combatAreas[area].monsters).concat(slayerAreaDisplayOrder.flatMap(area => slayerAreas[area].monsters));
+        for (let i = 0; i < slayerTasks.length; i++) {
+            $(`#${id}-container`).append(createImg(slayerTasks[i]));
         }
 
-        const loadedAutoEnabled = SEMI.getItem(`${id}-config`);
-        if(loadedAutoEnabled !== null) {
-            autoEnabled = [...loadedAutoEnabled];
-        }
-
-        fadeAll();
+        tippy(`#${id}-container [data-tippy-content]`, { animation: false });
     };
 
     const onDisable = () => {
@@ -68,53 +60,48 @@ var autoSellShow = (() => {
     const autoShow = () => {  $(`#modal-${id}`).modal('show'); };
 
     const injectGUI = () => {
-        const x = $('#modal-monster-log').clone().first();
-        // const guiCheckState = document.contains(document.getElementById(`modal-${id}`));
-        // if (guiCheckState) { $(`#modal-${id}`).remove(); }
-        x.attr('id', `modal-${id}`);
-        $('#modal-monster-log').parent().append(x);
-        const y = $(`#modal-${id}`).children().children().children().children('.font-size-sm');
+        autoEnabled = Array(MONSTERS.length).fill(false);
+        const loadedAutoEnabled = SEMI.getItem(`${id}-config`);
+        if(loadedAutoEnabled !== null) {
+            autoEnabled = [...loadedAutoEnabled];
+        }
+
+        const modal = $('#modal-monster-log').clone().first();
+        modal.attr('id', `modal-${id}`);
+        $('#modal-monster-log').parent().append(modal);
+        const y = modal.children().children().children().children('.font-size-sm');
         y.children().children().attr('id', `${id}-container`);
 
         const enableAutoButton = $(`<button class="btn btn-md btn-danger SEMI-modal-btn" id="${id}-status">Disabled</button>`);
         enableAutoButton.on('click', () => SEMI.toggle(id));
         y.before(enableAutoButton);
 
-        const refreshLogBtn = $(`<button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+        const toggleHiddenMonsters = $(`<button type="button" class="btn-block-option"><i class="far fa-eye" title="Show hidden monsters"></i></button>`).on('click', () => {
+            showHiddenMonsters = !showHiddenMonsters;
+            setupContainer();
+        });
+        const refreshLogBtn = $(`<button type="button" class="btn-block-option" aria-label="Refresh">
             <i class="fas fa-undo-alt text-muted" title="Refresh this log page to reflect your current monster log."></i>
             </button>`);
-        refreshLogBtn.on('click', () => SEMI.refreshMonsterLog());
-        $(`#${id}-status`).parent().find('.fa.fa-fw.fa-times').before(refreshLogBtn);
+        refreshLogBtn.on('click', () => setupContainer());
+        modal.find('.block-options').prepend(refreshLogBtn).prepend(toggleHiddenMonsters);
 
-        $(`#modal-${id} .block-title`).text(`${title} Menu`);
+        modal.find('.block-title').text(`${title} Menu`);
 
-        $(`#modal-${id}`).on('hidden.bs.modal', () => {
+        modal.find('#toggleMonsters').remove(); // Temporary fix for incompatibility with Completion Log Helper
+
+        modal.on('hidden.bs.modal', () => {
             SEMI.setItem(`${id}-config`, autoEnabled);
         });
+        setupContainer();
+
         setTimeout(() => {
-            setupContainer();
-            setTimeout(() => {
-                $(`#${id}-menu-status`)[0].innerHTML = '';
-                $(`#${id}-menu-button`).on('click', autoShow);
-                $(`#${id}-menu-button`).attr('href', null);
-            }, 1000);
-        }, 1000)
-
-    };
-
-    const refreshMonsterLog = () => {
-        $(".modal.show").find(".fa.fa-fw.fa-times").click();
-        $(`#modal-${id}`).remove();
-        injectGUI();
-        $(`#modal-${id}`).modal('show');
-        if (SEMI.isEnabled(id)) {
-            $(`#${id}-status`).addClass('btn-success');
-            $(`#${id}-status`).removeClass('btn-danger');
-            $(`#${id}-status`).text('Enabled');
-        }
+            $(`#${id}-menu-status`)[0].innerHTML = '';
+            $(`#${id}-menu-button`).on('click', autoShow);
+            $(`#${id}-menu-button`).attr('href', null);
+        }, 1000);
     };
 
     SEMI.add(id, {onLoop: doAll, onEnable, onDisable, title, desc, pluginType: SEMI.PLUGIN_TYPE.AUTO_COMBAT});
     SEMI.add(id + '-menu', {title, desc, imgSrc, injectGUI, pluginType: SEMI.PLUGIN_TYPE.AUTO_COMBAT});
-    SEMI.mergeOnto(SEMI,{refreshMonsterLog});
 })();
