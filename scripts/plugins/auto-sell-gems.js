478198b8b87c3a6a1bded50a2df9f30d511f41ca
AldousWatts <aldouswatts@protonmail.com>
2020-10-24 14:55:57 -0500

    ugly coded but working auto-sell-gems config

--- scripts/plugins/auto-sell-gems.js
+++ scripts/plugins/auto-sell-gems.js
@@ -7,25 +7,29 @@
     //AutoSellGems, scrapped from Melvor Super Control Panel by Strutty on Greasefork, since deleted
     //once it hits this amount, sell all of them.
     const config = {
-        targetStack: 100
+        targetStack: 100,
+        gemToggle: [true, true, true, true, true]
     };
 
-    const gemIdList = items.map((item, i) => ({...item, i})).filter((item) => item.type == 'Gem').map((x) => x.i); //ruby boobies & such
+    const gemIdList = items.map((item, i) => ({...item, i})).filter((item) => item.type == 'Gem').map((x) => x.i); //auto-get gemIDs from game code
 
     //AutoSellGems: Will sell gems when they reach the stack amount specified
     const autoSellGems = () => {
         var count = 0;
         const targetStack = SEMI.getValue(id, 'targetStack');
+        const toggleArray = SEMI.getValue(id, 'gemToggle');
+        let togIndex = 0;
         for(const gemId of gemIdList) {
             const curQty = SEMI.getBankQty(gemId);
             //console.log('GEM ID '+gemId+' you have '+curQty);
-            if(curQty > targetStack) {
+            if(curQty > targetStack && toggleArray[togIndex]) {
                 count++;
                 setTimeout(() => {
                     SEMI.sellItemWithoutConfirmation(gemId, targetStack);
                     SEMI.customNotify('assets/media/main/coins.svg', `Auto Sell Gems just sold ${targetStack} ${items[gemId].name}.`, 5000);
                 }, count*150);
             }
+            togIndex++;
         }
     };
 
@@ -36,9 +40,33 @@
         <!-- radios for each gem -->
         Choose gems to sell:
         <div class="custom-control custom-switch mb-1">
-            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-0" name="${id}-gem-radio-0">
-            <label class="custom-control-label" for="${id}-gem-radio-0">
-                <img src="${imgSrc}" class="nav-img" />
+            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-${gemIdList[0]}" name="${id}-gem-radio-${gemIdList[0]}">
+            <label class="custom-control-label" for="${id}-gem-radio-${gemIdList[0]}">
+                <img src="${items[gemIdList[0]].media}" class="nav-img" />
+            </label>
+        </div>
+        <div class="custom-control custom-switch mb-1">
+            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-${gemIdList[1]}" name="${id}-gem-radio-${gemIdList[1]}">
+            <label class="custom-control-label" for="${id}-gem-radio-${gemIdList[1]}">
+                <img src="${items[gemIdList[1]].media}" class="nav-img" />
+            </label>
+        </div>
+        <div class="custom-control custom-switch mb-1">
+            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-${gemIdList[2]}" name="${id}-gem-radio-${gemIdList[2]}">
+            <label class="custom-control-label" for="${id}-gem-radio-${gemIdList[2]}">
+                <img src="${items[gemIdList[2]].media}" class="nav-img" />
+            </label>
+        </div>
+        <div class="custom-control custom-switch mb-1">
+            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-${gemIdList[3]}" name="${id}-gem-radio-${gemIdList[3]}">
+            <label class="custom-control-label" for="${id}-gem-radio-${gemIdList[3]}">
+                <img src="${items[gemIdList[3]].media}" class="nav-img" />
+            </label>
+        </div>
+        <div class="custom-control custom-switch mb-1">
+            <input type="checkbox" class="custom-control-input" id="${id}-gem-radio-${gemIdList[4]}" name="${id}-gem-radio-${gemIdList[4]}">
+            <label class="custom-control-label" for="${id}-gem-radio-${gemIdList[4]}">
+                <img src="${items[gemIdList[4]].media}" class="nav-img" />
             </label>
         </div>
     </div>`;
@@ -47,12 +75,27 @@
         if ($(`#${id}-stack-form`).val() === "") stackVal = config.targetStack;
         if (stackVal !== null && !isNaN(stackVal) && stackVal>0) {
             SEMI.setValue(id, 'targetStack', stackVal);
-            SEMI.setItem(`${id}-config`, SEMI.getValues(id) );
-            SEMI.customNotify(imgSrc, `Saved AutoSellGems targetStack: ${stackVal}`, 3000);
         }
-    }
+        // const toggled = $(`#${id}-gem-radio-${gemIdList[gem]}`).attr('checked');
+        let toggleArray = [];
+        let count = 0;
+        gemIdList.forEach(x => {
+            const checked = $(`#${id}-gem-radio-${x}`).prop('checked');
+            toggleArray[count] = checked;
+            count++;
+        });
+        SEMI.setValue(id, 'gemToggle', toggleArray);
+        SEMI.setItem(`${id}-config`, SEMI.getValues(id) );
+        SEMI.customNotify(imgSrc, `Saved AutoSellGems config!`, 3000);
+    };
     const updateConfig = () => {
         $(`#${id}-stack-form`).val(SEMI.getValue(id, 'targetStack'));
-    }
+        const toggleArray = SEMI.getValue(id, 'gemToggle');
+        let count = 0;
+        gemIdList.forEach(x => {
+            $(`#${id}-gem-radio-${x}`).prop('checked', toggleArray[count]);
+            count++;
+        });
+    };
     SEMI.add(id, {ms: 5000, onLoop: autoSellGems, desc, title, imgSrc, config, hasConfig, configMenu, saveConfig, updateConfig});
 })();
