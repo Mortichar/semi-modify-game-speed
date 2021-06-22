b091775e6db25e46f2c453bbc60a2d2ee0add34f
Chris Dayton <chrisdayt@gmail.com>
2021-06-22 18:26:30 -0500

    rock reset time, new combat hook

--- scripts/plugins/game-speed.js
+++ scripts/plugins/game-speed.js
@@ -5,7 +5,7 @@
     const imgSrc = 'assets/media/bank/skillcape_max.svg';
     const version = 1;
     const defaultGameSpeed = 1;
-    const maximumSpeed = 5;
+    const maximumSpeed = 10;
     const minimumSpeed = 1;
 
     const clamp = (value, minimum, maximum) => {
@@ -59,6 +59,8 @@
     const updatePlayerAttackSpeedOriginal = updatePlayerAttackSpeed;
     const magicIntervalOriginal = magicInterval;
     const loadNewEnemyOriginal = loadNewEnemy;
+    const startCombatOriginal = startCombat;
+    const rockResetOriginal = rockReset;
 
     const injectGUI = () => {
         console.log('Attempting to modify Main.js calculatureSkillInterval');
@@ -79,6 +81,19 @@
         SEMI.replaceGameFunc('calculateSkillInterval', calculateSkillIntervalNew);
         console.log('Successfully adjusted calculateSkillInterval.');
 
+        console.log('Attempting to just rock respawn interval . . .');
+        var rockResetNew = rockResetOriginal.toString();
+        rockResetNew = rockResetNew.replace(
+            'miningData[ore].respawnInterval;',
+            `miningData[ore].respawnInterval/${config.gameSpeed};`
+        );
+        if (rockResetNew == rockResetOriginal.toString()) {
+            console.rerror('Failed to adjust rock respawn interval!');
+            return;
+        }
+        SEMI.replaceGameFunc('rockReset', rockResetNew);
+        console.log('Successfully adjusted rock respawn interval.');
+
         console.log('Attempting to adjust alt magic interval . . .');
         magicInterval = magicIntervalOriginal / config.gameSpeed;
         console.log('Successfully adjusted alt magic interval.');
@@ -92,6 +107,17 @@
             combatData.player.baseAttackSpeed = combatData.player.baseAttackSpeed / config.gameSpeed;
             updatePlayerAttackSpeedOriginal();
         };
+
+        startCombatNew = startCombat.toString();
+        startCombatNew = startCombatNew.replace(
+            'attackInterval=playerSpecialAttacks[specID].attackInterval;',
+            `attackInterval=playerSpecialAttacks[specID].attackInterval/${config.gameSpeed};`
+        );
+        if (startCombatNew == startCombat.toString()) {
+            console.error('Failed to update player attack speed!');
+            return;
+        }
+        SEMI.replaceGameFunc('startCombat', startCombatNew);
         console.log('Successfully adjusted combat attack speed for the player.');
 
         console.log('Attemping to adjust combat attack speed for enemies . . .');
@@ -112,6 +138,14 @@
             console.error('Failed to adjust enemy attack speed!');
             return;
         }
+        loadNewEnemyNew = loadNewEnemyNew.replace(
+            'enemySpawnTimer-playerModifiers.decreasedMonsterRespawnTimer+playerModifiers.increasedMonsterRespawnTimer',
+            `(enemySpawnTimer-playerModifiers.decreasedMonsterRespawnTimer+playerModifiers.increasedMonsterRespawnTimer)/${config.gameSpeed};`
+        );
+        if (loadNewEnemyNew == loadNewEnemyOriginal.toString()) {
+            console.error('Failed to adjust enemy attack speed!');
+            return;
+        }
         SEMI.replaceGameFunc('loadNewEnemy', loadNewEnemyNew);
         console.log('Successfully adjusted combat attack speed for enemies.');
     };
@@ -126,10 +160,15 @@
 
         // restore player attack interval
         updatePlayerAttackSpeed = updatePlayerAttackSpeedOriginal;
+        SEMI.removeReplacementFunc('startCombat');
+        startCombat = startCombatOriginal;
 
         // restore enemy attack speed interval
         SEMI.removeReplacementFunc('loadNewEnemy');
         loadNewEnemy = loadNewEnemyOriginal;
+
+        SEMI.removeReplacementFunc('rockReset');
+        rockReset = rockResetOriginal;
     };
 
     SEMI.add(id, {
